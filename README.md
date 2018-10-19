b63ad23dbf14f3b8284943d8afb6a7a01c97d424
Theo Daly <theo.daly@outlook.com>
2018-10-18 23:39:36 -0500

    all files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # TrainSchedule
+## What is this repo project?
+
+* Train schedule application that incorporates Firebase to host arrival and departure data. 
+
+## How does it work?
+
+* Retrieve and manipulate this information with Moment.js. 
+* This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+## Who will use this repo?
+
+* This is for coursework utilizing firbase, but is open to anyone who wants to explore the code or play with it for fun.
+
+## What is the goal of the project?
+
+* When adding trains, administrators should be able to submit the following:
+    
+    * Train Name
+    
+    * Destination 
+    
+    * First Train Time -- in military time
+    
+    * Frequency -- in minutes
+  
+  * Code this app to calculate when the next train will arrive; this should be relative to the current time.
+  
+  * Users from many different machines must be able to view same train times.
---
+++ app.js
@@ -0,0 +1,116 @@
+var config = {
+    apiKey: "AIzaSyA1aIjY6vE9UOIOtMx31nIq_8b-Vu5zTdw",
+    authDomain: "trainschedule-ed82c.firebaseapp.com",
+    databaseURL: "https://trainschedule-ed82c.firebaseio.com",
+    projectId: "trainschedule-ed82c",
+    storageBucket: "trainschedule-ed82c.appspot.com",
+    messagingSenderId: "967186088183"
+};
+firebase.initializeApp(config);
+
+// references firebase
+var database = firebase.database(),
+
+    name = name,
+    trainName = trainName,
+    destination = destination,
+    time = time,
+    frequency = frequency,
+    nextTrain = nextTrain,
+    minutesAway = minutesAway;
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    computeValues();
+});
+
+function computeValues() {
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    time = $("#time").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    // adds a zero to times with one digit so they will be military time I think
+    if (time.match(/\D/).index === 1) {
+        time = "0" + time;
+    }
+
+
+    var timeRightNow = moment().format("YYYY-MM-DD HH:mm"),
+        // convert time entered to match current time format
+        formattedTime = moment().format("YYYY-MM-DD") + " " + time;
+
+    // Set variable with next train time and format 0 to midnight
+    function nextTrainTime() {
+        nextTrain = moment(formattedTime).format("HH:mm A");
+        if (nextTrain === "00:00 AM") {
+            nextTrain = "12:00 AM";
+        }
+    }
+
+    // Calculate next arrival
+    if (formattedTime > timeRightNow) {
+        nextTrain = time;
+        minutesAway = moment(formattedTime).diff(moment(timeRightNow), "minutes");
+        nextTrainTime();
+    }
+    else {
+        while (formattedTime < timeRightNow) {
+
+            var incrementTime = moment(formattedTime).add(frequency, "minutes"),
+                newTime = moment(incrementTime._d).format("YYYY-MM-DD HH:mm");
+            formattedTime = newTime;
+        }
+        nextTrainTime();
+        // Set variable with difference of next train and current time
+        minutesAway = moment(formattedTime).diff(moment(timeRightNow), "minutes");
+    }
+
+    // Convert minutesAway to military time
+    if (minutesAway > 60) {
+        if (minutesAway % 60 === 0) { //60 cause of hours
+            minutesAway = Math.floor(minutesAway / 60) + " hours"
+        }
+        else {
+            minutesAway = Math.floor(minutesAway / 60) + "h " + minutesAway % 60 + "m";
+        }
+    }
+    else {
+        minutesAway = minutesAway + " minutes";
+    }
+
+    // Change frequency to military
+    if (frequency > 60) {
+        if (frequency % 60 === 0) {
+            frequency = Math.floor(frequency / 60) + " hours"
+        }
+        else {
+            frequency = Math.floor(frequency / 60) + "h " + frequency % 60 + "m";
+        }
+    }
+    else {
+        frequency = frequency + " minutes";
+    }
+
+    // Push to database
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        nextTrain: nextTrain,
+        minutesAway: minutesAway
+    });
+}
+//Append values to be displayed on my table
+database.ref().on("child_added", function (childSnapshot) {
+
+    $("#trainSchedule").append("<tr>" +
+        "<td>" + childSnapshot.val().trainName + "</td>" +
+        "<td>" + childSnapshot.val().destination + "</td>" +
+        "<td>" + childSnapshot.val().frequency + "</td>" +
+        "<td>" + childSnapshot.val().nextTrain + "</td>" +
+        "<td>" + childSnapshot.val().minutesAway + "</td>" +
+        "</tr>"
+    );
+
+});
